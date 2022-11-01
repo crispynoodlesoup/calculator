6c853f5cd34306d1e7e1ca73e3a895c7f1273018
crispynoodlesoup <thepanman321@gmail.com>
2022-10-31 21:26:51 -0500

    split display to main and auxiliary lines

--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
-const display =  document.querySelector("#display");
+const display = document.querySelector("#display");
+const auxiliaryText = document.querySelector("#display-previous");
+const mainText = document.querySelector("#display-input");
 const displayLength = 12;
-let dStr = "91827334";
+let auxStr = "129440 * 129740 =";
+let mainStr = "129470983740";
+
+auxiliaryText.innerText = auxStr;
 
 const updateDisplay = () => {
-    if(dStr.length > displayLength)
-        dStr = dStr.substring(0, displayLength);
-    display.innerText = dStr;
+    if(mainStr.length > displayLength)
+        mainStr = mainStr.substring(0, displayLength);
+    mainText.innerText = mainStr;
 };
 
 const add = (a, b) => a + b;
