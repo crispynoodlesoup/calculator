c336b248b41275b47744d5520be3e6471b71c207
crispynoodlesoup <thepanman321@gmail.com>
2022-11-02 18:06:08 -0500

    add negative and decimal functionality

--- script/script.js
+++ script/script.js
@@ -3,8 +3,7 @@ const auxiliaryText = document.querySelector("#display-previous");
 const mainText = document.querySelector("#display-input");
 const displayLength = 12;
 let shift = false;
-let erase = true;
-let decimal = false;
+let erase = false;
 let operation = "";
 let auxStr = "";
 let mainStr = "";
@@ -19,17 +18,17 @@ const updateDisplay = () => {
     mainText.innerText = mainStr;
 };
 
-const add = (a, b) => Number.parseFloat(a) + Number.parseFloat(b);
-const subtract = (a, b) => Number.parseFloat(a) - Number.parseFloat(b);
-const multiply = (a, b) => Number.parseFloat(a) * Number.parseFloat(b);
-const divide = (a, b) => Number.parseFloat(a) / Number.parseFloat(b);
+const add = (a, b) => formatNum(Number.parseFloat(a) + Number.parseFloat(b));
+const subtract = (a, b) => formatNum(Number.parseFloat(a) - Number.parseFloat(b));
+const multiply = (a, b) => formatNum(Number.parseFloat(a) * Number.parseFloat(b));
+const divide = (a, b) => formatNum(Number.parseFloat(a) / Number.parseFloat(b));
 const operate = () => {
     if (operation != "") {
         let operand2 = mainStr;
         let result = "ERROR";
-        if(operation === "-") 
+        if (operation === "-")
             result = subtract(operand, operand2);
-        if(operation === "+")
+        if (operation === "+")
             result = add(operand, operand2);
 
         console.log(result);
@@ -48,64 +47,75 @@ const inputKeyboard = (e) => {
         mainStr = mainStr.substring(0, mainStr.length - 1);
 
     // erase display
-    if(erase && ((e.keyCode <= 57 && e.keyCode >= 48) || e.keyCode == 8)) {
+    if (erase && ((e.keyCode <= 57 && e.keyCode >= 48) || e.keyCode == 8)) {
         mainStr = "";
         erase = false;
     }
-    
+
     // digits 0-9
     if (e.keyCode <= 57 && e.keyCode >= 48)
         mainStr += `${e.keyCode - 48}`;
-    
+
     // minus
     if (e.keyCode === 189) {
-        erase = false;
-        operation = "-";
-        if(!operand && mainStr) {
-            operand = mainStr;
-            mainStr = "";
-            auxStr = `${operand} -`;
-        } else if(!mainStr && operand ) {
-            auxStr = `${operand} -`;
-        } else if(mainStr && operand)
-            operate();
+        if (!mainStr) {
+            mainStr += "-";
+        } else {
+            erase = false;
+            operation = "-";
+            if (!operand && mainStr) {
+                operand = mainStr;
+                mainStr = "";
+                auxStr = `${operand} -`;
+            } else if (!mainStr && operand) {
+                auxStr = `${operand} -`;
+            } else if (mainStr && operand)
+                operate();
+        }
     }
-    
+
     // plus
     if (e.keyCode === 187 && shift) {
         erase = false;
         operation = "+";
-        if(!operand && mainStr) {
+        if (!operand && mainStr) {
             operand = mainStr;
             mainStr = "";
             auxStr = `${operand} +`;
-        } else if(!mainStr && operand ) {
+        } else if (!mainStr && operand) {
             auxStr = `${operand} +`;
-        } else if(mainStr && operand)
+        } else if (mainStr && operand)
             operate();
     }
 
     // enter
-    if (e.keyCode === 13 && mainStr && operand) {
+    if (e.keyCode === 13 && Boolean(Number(mainStr)) && operand) {
         erase = false;
         operate();
         operation = "";
     }
 
+    // period
+    if (e.keyCode === 190 && !mainStr.includes(".")) {
+        if (!mainStr)
+            mainStr += "0";
+        mainStr += ".";
+    }
+
     // shift
-    if(e.keyCode === 16)
+    if (e.keyCode === 16)
         shift = true;
-    
+
     updateDisplay();
 };
 
 const letGoShift = (e) => {
-    if(e.keyCode == 16)
+    if (e.keyCode == 16)
         shift = false;
 };
 
 const formatNum = (num) => {
-    if(Number.isInteger(num))
+    if (Number.isInteger(num))
         return num;
     return num.toFixed(3);
 }
